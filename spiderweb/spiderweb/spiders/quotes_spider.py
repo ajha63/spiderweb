5c0ec6b3c648fb9aaa1336c57753c29ac020021b
Alvaro Hernandez <alvaro.hernandez@vnc.biz>
2017-07-04 18:50:13 -0400

    Following links

--- spiderweb/spiderweb/spiders/quotes_spider.py
+++ spiderweb/spiderweb/spiders/quotes_spider.py
@@ -3,14 +3,9 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-
-    def start_requests(self):
-        urls = [
-                'http://quotes.toscrape.com/page/1/',
-                'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
 
     def parse(self, response):
         for quote in response.css('div.quote'):
@@ -20,3 +15,6 @@ class QuotesSpider(scrapy.Spider):
                 'tags': quote.css('div.tags a.tag::text').extract(),
             }
 
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
