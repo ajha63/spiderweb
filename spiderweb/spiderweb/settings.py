7f4d513c5d27a291313c01f5120380bc4d5e0ea0
Alvaro Hernandez <alvaro.hernandez@vnc.biz>
2017-07-05 12:30:28 -0400

    Set user agent

--- spiderweb/spiderweb/settings.py
+++ spiderweb/spiderweb/settings.py
@@ -17,6 +17,7 @@ NEWSPIDER_MODULE = 'spiderweb.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'spiderweb (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
